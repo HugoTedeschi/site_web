0baa61b913b1145a37938a7b6ed0530a24eea6d7
Hugo Tedeschi <hugo.tedeschi@ens.uvsq.fr>
2019-12-10 14:28:20 +0100

    Modif de acceuil

--- acceuil.php
+++ acceuil.php
@@ -26,31 +26,29 @@
    
    <hr>
 
+  
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Notre histoire</h5>
+    <p> Nous sommes deux amis qui se sont rencontré par hasard pendant leur études. C'est l'association deux aventurier à la fois de la musique mais aussi de dans le milieu des start-up. C'est ansin qu'à partir d'une passion commune, Abdelmajid et Hugo se sont lancé dans le commerce d'instruments de musique</p>
+  </div>
+  
+  
+  
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Notre premier magasin</h5>
+    <p>Nous avons lancé notre premier magasin à Paris. Nous avons toujours privilégié un rapport humain entre nos vendeurs et nos client. C'est avant tout essentiel pour nous. C'est pourquoi nous avons implanté plsuieurs magasin en France pour satisfaire notre clientèle.</p>
     
      <div class="divider"></div>
   <div class="section">
-    <h5>Qui sommes-nous ?</h5>
-    <p>Stuff</p>
-  </div>
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Notre histoire:</h5>
-    <p>Stuff</p>
-  </div>
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Notre premier magasin:</h5>
-    <p>Stuff</p>
-    
-     <div class="divider"></div>
-  <div class="section">
-    <h5>Abdelmajid Omarjee:</h5>
-    <p>Stuff</p>
+    <h5>Abdelmajid Omarjee</h5>
+    <p>Abdelmajid Omarjee , diplomé d'école de commerce et passioné de musique. Il lance une première entreprise dans le commerce de chèvre avant de basculer dans l'agro-alimentaire. Enfin, il finit par trouver son métier de coeur: la musique</p>
     
      <div class="divider"></div>
   <div class="section">
     <h5>Hugo Tedeschi</h5>
-    <p>Stuff</p>
+    <p>Hugo Tedeschi, musicien de formation, il se lance dans la musique en commençant par de petites scène sur Paris puis pas des concert partout en france.  </p>
   </div>
     
     </body>
