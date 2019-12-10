f2e5a922208323852f5eebc4158d66f2ce28d579
Utilisateur <user@debian.arcahe.ovh>
2019-12-10 20:17:46 +0100

    identification.php

--- identification.php
+++ identification.php
@@ -8,18 +8,18 @@
     </head>
 
      
-	<div id style='corps'>
+	
     <body>
-		
+			
         <h2>Connectez vous en un instant !</h2>
-
-        <form method="POST" action="acceuil.php">
+		<?php include("menu.php"); ?>
+        <form method="POST" action="identification1.php">
             <input type="text" name="login" maxlength="20" placeholder="Login" style="text-transform:capitalize;"></input></br>
             <input type="password" name="mdp" maxlength="20" placeholder="Mot de passe"></input><br/>        
             <input type="submit" name="ok" value="Valider" >
         </form>
 
-       </div>
+      
     </body>
      
 </html>
