feb00e4d82c34195afdeb5c6fd4ab9568eb71cb8
Utilisateur <user@debian.arcahe.ovh>
2019-12-10 09:21:08 +0100

    AJout dez nouveaux fichier site web

---
+++ acceuil.php
@@ -0,0 +1,36 @@
+<html>
+	
+    <head>
+        <title>Page d'Accueil </title>
+        <link href='style.css' rel='stylesheet' type='text/css'>
+        <meta charset="utf-8" />
+    </head>
+
+    <body>
+    
+     
+    <h2>Bienvenue sur la Fanfare en ligne, le site des mille et une note</h2>
+       <?php include("menu.php"); ?>
+   
+    <p> Nous proposons sur notre site internet toute notre collection d'instruments ! Mais vous pouvez aussi retrouvers bien d'autres informations essentielles telles que: <br>
+    <list>
+    <li>Nos fournisseurs: Fender, Gibson, Kumalae et bien d'autres illustres ateliers, connus à travers le monde <br></li>
+    <li>Toutes les adresses de nos magasins situés en France. <br></li>
+    </list>
+    </p>
+   
+   <hr>
+    <h2> Qui sommes-nous ?</h2>
+     <p> <br>
+    <list>
+    <li> Notre histoire: <br></li>
+    <li> Notre premier magasin: <br></li>
+    <li> Abdel: <br></li>
+    <li> Hugo <br></li>
+    </list>
+    </p>
+    
+    
+    </body>
+    
+</html>
---
+++ menu.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="style.css" />
+
+<html>
+
+<!--
+ <nav>
+<li><a>Accueil</a></li>
+<li><a>Catalogue d'instruments</a></li>
+<li><a>Nos Magasins</a></li>
+<li><a>Nos Fournisseurs</a></li>
+
+</nav> 
+-->
+<TABLE BORDER="2">
+<TR>
+<TH> Catalogue d'instruments </TH>
+<TH> Nos fournisseurs </TH>
+<TH> Nos magasins </TH>
+<TH> Mon compte</TH>
+</TR>
+</TABLE>
+     
+   
+</html>
