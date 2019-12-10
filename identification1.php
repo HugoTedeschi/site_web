1a230ef091a1b3d156b805423dc207ea38758667
Utilisateur <user@debian.arcahe.ovh>
2019-12-10 20:13:54 +0100

    identification1

---
+++ identification1.php
@@ -0,0 +1,59 @@
+
+<html>
+
+    <head>
+        <title>Page de confirmation </title>
+        <meta charset="utf-8" />
+    </head>
+
+	
+	
+    <body>
+
+    	<?php 
+			$link = new mysqli("localhost","user","user");
+			if ($link->connect_errno){
+			  die("Couldn't connect to MySQL ".$link->connect_error);
+			}
+			
+			$link->select_db("madata");
+	        $login=$_Post['login'];
+	        $mdp=$_Post['mdp'];
+			$sql_pseudos = "SELECT identifiant FROM Compte WHERE '$login' = identifiant AND '$mdp' = mdp"; // On select le couple correspondant dans la BDD
+			//var_dump($sql_pseudos);
+			$pseudos_existants = $link->query($sql_pseudos) 
+				or die("pb de requetes ".$link->error);
+
+			if($pseudos_existants ->num_rows){
+				
+				//Si le pseudo et le mot de passe correspondent tout va bien
+				//session_start(); // LANCEMENT SESSION
+				//$_SESSION['pseudo'] = $login; // CREATION VARIABLE GLOBALE login
+				//$aa = $CodeCompagnie->fetch_row();
+				//$_SESSION['CodeCompagnie'] = $aa[0];  // CREATION VARIABLE GLOBALE CODE CIE
+			echo "Bon retour parmis nous "; echo "$login"; echo"!";
+				
+			}
+
+			else{
+				//Si la combinaison n'existe pas on affiche un message d'erreur
+				echo "Il semblerait que le mot de passe et le login ne correspondent pas !";
+				$aa = "POST";
+				$ab = "identification.php";
+				$ac = "submit";
+				$ad = "retour";
+				$ae = "Retour au formulaire";
+			    $texte = "<form method='$aa'action='$ab'>
+			    <input type='$ac' name='$ad' value='$ae' >
+        		</form>";
+        		echo "$texte";
+			}
+			$link->close();
+			
+			$essai = '<META HTTP-EQUIV="Refresh" CONTENT="15,acceuil.php">';
+				echo "$essai";
+  		?>
+        
+    </body>
+   
+</html>
