ec18790516145a955d379b05c0ffddd8c8f2215b
Hugo Tedeschi <hugo.tedeschi@ens.uvsq.fr>
2019-12-11 22:02:55 +0100

    modif catalogue presque fonctionnel

--- catalogue.php
+++ catalogue.php
@@ -1,22 +1,28 @@
 <head>
 <title> Catalogue</title>
+
+
  <link href='style.css' rel='stylesheet' type='text/css'>
 <link <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 …
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <?php include("menu.php"); ?>
-<h2>Bienvenue sur notre catalogue en ligne !</h2>
-<p    Vous retrouverez ici tous nos instruments !  text = center</p>
+
 	
-
-
-
-
 </head>
 
-<body>
 
+<body style='background:black; color:white;'>
+	
+	<h2>Bienvenue sur notre catalogue en ligne !</h2>
+		<form action="catalogue.php" >
+		<center>	<input type="submit" value="Revenir au catalogue"  style='width:500px; background:grey; padding : 20px; font-size: 40px; text-align: center;' /></center>
+		</form>
+	<p style='text-align: center;font-size: 25px'>  Vous retrouverez ici tous nos instruments ! </p>
+
+
+	
 <?php
 
 
@@ -33,18 +39,14 @@ if ($link->connect_errno) {
  * 
  */
 $prix=0;
-$marque ="";
-$modele ="";
+$marque ='rien';
+$modele ='rien';
 
-$result_prix = $link->query( "SELECT modele, type, nom_fourn, prix, SUM(en_stock) AS Nombre
+$catalogue = $link->query( "SELECT modele, type, nom_fourn, prix, SUM(en_stock) AS Nombre
 FROM liste_stockes
 GROUP BY modele, type, nom_fourn
 ORDER BY prix;" )
 or die("SELECT Error: ".$link->error);
-
-
-
-
 
 
 $result_marque = $link->query( "(SELECT modele, type, nom_fourn, prix, SUM(en_stock) AS Nombre
@@ -76,61 +78,71 @@ GROUP BY modele, type, nom_fourn
 ORDER BY prix;" )
 or die("SELECT Error: ".$link->error);
 
+
+
 print "
 
-
- <p2>
- 
- <button onclick='envoyer(0);'> bouton test</button>
- </p2>
+	<table style='background:black; color:white;'>
 		<br>
+		<td>
+         <form method = 'post'>  
+           
+              
+            <select name = 'choix_marque'style='background:black; color:white;'>   
+                <option value = 'rien'>Aucune marque selectionnée</option> 
+                <option value = 'Fender'>Fender</option> 
+                <option value = 'Gibson'>Gibson</option> 
+                <option value = 'Kumalae'>Kumalae</option> 
+                <option value = 'Selmer'>Selmer</option> 
+                <option value = 'Yamaha'>Yamaha</option> 
+                <option value = 'Hohner'>Hohner</option> 
+            </select> 
+            <input type = 'submit' name = 'submit' value = Submit> 
+        </form> 
+	    </td>
 		
-        Voir les instruments d'une marque particulière:
-        <form method='POST'>
-		<select id='marque' name='marque' size='1' tabindex='5'>
-		<option value='0'>pas de selection particulière
-		<option value='1'>Fender
-		<option value='2'>Gibson
-		<option value='3'>Yamaha
-		<option value='3'>Kumalae
-		<option value='3'>Selmer
-		<option value='3'>Hohner
-		<option value='3'>Catania
-		</select><br>
-		<br>
-		
-		</form>
 	
-		<button onclick='envoyer(9);'></button>
+	</table>
 		
 		
        
 
 ";
-if (isset($_POST['marque']))
-{
-	
-	$choix = $_POST['marque'];
-	if ($choix==1)
-	{
-		echo "on a ecrit fender";
-	}
-	elseif ($choix==2)
-	{
-		echo "on a ecrit gibson";
-	}
-	elseif ($choix==3)
-	{
-		echo "on a ecrit Yamaha";
-	}
-}
+  if(isset($_POST["submit"]))  
+    { 
+        // Check if any option is selected 
+        if(isset($_POST['choix_marque']))  
+        { 
+			$marque = $_POST['choix_marque'];
+              
+                print "vous avez choisis de voir les instruments de la marque $marque<br/>"; 
+        } 
+    else
+        echo "Aucune marque selectionnée, retour au catalogue des instruments."; 
+    } 
 
-print "<table width=200 border=1>\n";
 
-print "<form id='FormulaireCommande' method='POST' action='commande.php' 
- <input type='reset' value='Recommancer la commande'>
- <input type='reset' value='Recommancer la commande'>
-		<input type='submit' value='Passer la commande'>    ";
+
+print "<table width=200 border=1   style='background:black; color:white;'>\n";
+
+
+print "<form id='FormulaireCommande' method='POST' action='commande.php'";
+
+
+
+print "<tr>\n";
+
+ print "\t<td><input type='reset' value='Recommancer la commande' style='width:350px; background:grey;'></td>\n";
+ print "\t<td></td>\n";
+ print "\t<td></td>\n";
+ print "\t<td></td>\n";
+ print "\t<td></td>\n";
+ print "\t<td><input type='submit' value='Passer la commande' style='width:350px; background:grey;'></td>\n";
+    
+  
+ print "</tr>\n";
+ 
+ 
 
 
 print "<tr>\n";
@@ -150,19 +162,31 @@ $value = "Modèle";
   
  print "</tr>\n";
  
- 
+ if($marque == 'rien')
+ {
+	 $result = $catalogue;
+ }
+ else
+ {
+	 $result = $result_marque ;
+	 echo "$marque";
+ }
        		
-while ($get_info = $result_prix->fetch_row()){
+while ($get_info = $result->fetch_row()){
  print "<tr>\n";
  
  
  
  
  // en gros faudrait pouvoir récupérer la valeur de get_info[0], correspondant à l'indice de l'instru.
- foreach ($get_info as $field)  // on fait indice par indice pour le sous tableau (une ligne de la table en gros)
- print "\t<td> $field</td>\n";
- 
+ foreach ($get_info as $field)
+ {  // on fait indice par indice pour le sous tableau (une ligne de la table en gros)
+	print "\t<td> $field</td>\n";
+ }
  print "\t<td> <input type='checkbox' name= 'test' value='$get_info[0]'></td>\n";
+ 
+ 
+ 
  print "</tr>\n"; // permet de séparer les lignes de la table entres-elles.
 }
 
@@ -170,10 +194,7 @@ while ($get_info = $result_prix->fetch_row()){
 print "</form>";
 print "</table>\n";
 
-$result_prix-->free();
-
-
-
+$result-->free();
 $link->close();
 ?>
 
--- fournisseurs.php
+++ fournisseurs.php
@@ -1,9 +1,34 @@
-
-<html>
-
-
-<input type="checkbox" name="vehicle1" value="Bike"> I have a bike<br>
-<input type="checkbox" name="vehicle2" value="Car"> I have a car<br>
-<input type="checkbox" name="vehicle3" value="Boat" checked> I have a boat<br>
-
-</html>
+<html> 
+    <body> 
+        <!--name.php to be called on form submission--> 
+        <form method = 'post'>  
+            <h4>SELECT SUJECTS</h4> 
+              
+            <select name = 'subject[]' multiple size = 6>   
+                <option value = 'english'>ENGLISH</option> 
+                <option value = 'maths'>MATHS</option> 
+                <option value = 'computer'>COMPUTER</option> 
+                <option value = 'physics'>PHYSICS</option> 
+                <option value = 'chemistry'>CHEMISTRY</option> 
+                <option value = 'hindi'>HINDI</option> 
+            </select> 
+            <input type = 'submit' name = 'submit' value = Submit> 
+        </form> 
+    </body> 
+</html> 
+<?php 
+      
+    // Check if form is submitted successfully 
+    if(isset($_POST["submit"]))  
+    { 
+        // Check if any option is selected 
+        if(isset($_POST["subject"]))  
+        { 
+            // Retrieving each selected option 
+            foreach ($_POST['subject'] as $subject)  
+                print "You selected $subject<br/>"; 
+        } 
+    else
+        echo "Select an option first !!"; 
+    } 
+?> 
