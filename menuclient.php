c44be56e9c4490d7fa516cf608e20f27d7582fc2
Utilisateur <user@debian.arcahe.ovh>
2019-12-13 17:29:34 +0100

    efverfv

--- menuclient.php
+++ menuclient.php
@@ -9,7 +9,7 @@
 <TH> <a href='fournisseurs.php'> Nos fournisseurs</a></TH>
 <TH> <a href='magasin.php'> Nos magasins </TH>
 <TH> <a href='facture.php'> Mes facture</a></TH>
-<TH> <a href='identification.php' >Mon compte</a></TH>
+<TH> <a href='modificationcompte.php' >Mon compte</a></TH>
 </TR>
 </TABLE>
      
